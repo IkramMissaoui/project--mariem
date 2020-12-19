3104d16f33e71a49c3216b911b82fd08e40730da
IkramMissaoui <missaoui.ikram726@gmail.com>
2020-12-19 08:53:58 +0100

    first commit

---
+++ main2.js
@@ -0,0 +1,29 @@
+let heart=document.getElementById('heart')
+let heartt=document.getElementById('heartt')
+let hearttt=document.getElementById('hearttt')
+function heart1(){
+   if(heart.style.color=='red'){
+     heart.style.color='black'
+   }
+   else{
+     heart.style.color='red'
+   }
+ }
+
+ function heart2(){
+   if(heartt.style.color=='red'){
+      heartt.style.color='black'
+   }
+   else{
+     heartt.style.color='red'
+   }
+ }
+
+ function heart3(){
+   if(hearttt.style.color=='red'){
+     hearttt.style.color='black'
+   }
+   else{
+     hearttt.style.color='red'
+   }
+ }
